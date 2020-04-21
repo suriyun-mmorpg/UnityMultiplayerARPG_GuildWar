b416a6772607ae3a72e8dbeefdc03560dc873a2e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-04-22 01:45:02 +0700

    Add guild war monster character entity (may use it as castle guardian)

--- Scripts/GWCastleHeart.cs
+++ Scripts/GWCastleHeart.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 namespace MultiplayerARPG.MMO.GuildWar
 {
-    public class GWCastleHeart : MonsterCharacterEntity
+    public class GWCastleHeart : GWMonsterCharacterEntity
     {
+
         public override void Killed(IGameEntity lastAttacker)
         {
             base.Killed(lastAttacker);
 
             // Get winner guild id
-            int winnerGuildId = 0;
-            BasePlayerCharacterEntity playerCharacter = lastAttacker.Entity as BasePlayerCharacterEntity;
-            BaseMonsterCharacterEntity monsterCharacter = lastAttacker.Entity as BaseMonsterCharacterEntity;
-            if (monsterCharacter != null && monsterCharacter.IsSummoned &&
-                monsterCharacter.Summoner is BasePlayerCharacterEntity)
-                playerCharacter = monsterCharacter.Summoner as BasePlayerCharacterEntity;
-            if (playerCharacter != null)
-                winnerGuildId = (lastAttacker.Entity as BasePlayerCharacterEntity).GuildId;
+            int winnerGuildId = GetGuildId(lastAttacker);
 
             // Teleport other guild characters to other map (for now, teleport to respawn position)
             List<BasePlayerCharacterEntity> otherGuildCharacters = new List<BasePlayerCharacterEntity>(CurrentGameManager.GetPlayerCharacters());
---
+++ Scripts/GWMonsterCharacterEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG.MMO.GuildWar
+{
+    public class GWMonsterCharacterEntity : MonsterCharacterEntity
+    {
+        public int GetGuildId(IGameEntity entity)
+        {
+            int guildId = 0;
+            if (entity != null)
+            {
+                BasePlayerCharacterEntity playerCharacter = entity.Entity as BasePlayerCharacterEntity;
+                BaseMonsterCharacterEntity monsterCharacter = entity.Entity as BaseMonsterCharacterEntity;
+                if (monsterCharacter != null && monsterCharacter.IsSummoned &&
+                    monsterCharacter.Summoner is BasePlayerCharacterEntity)
+                    playerCharacter = monsterCharacter.Summoner as BasePlayerCharacterEntity;
+                if (playerCharacter != null)
+                    guildId = (entity.Entity as BasePlayerCharacterEntity).GuildId;
+            }
+            return guildId;
+        }
+
+        public override void ReceiveDamage(IGameEntity attacker, CharacterItem weapon, Dictionary<DamageElement, MinMaxFloat> damageAmounts, BaseSkill skill, short skillLevel)
+        {
+            if (!IsServer || IsDead() || GetGuildId(attacker) == 0 || !CanReceiveDamageFrom(attacker))
+                return;
+
+            base.ReceiveDamage(attacker, weapon, damageAmounts, skill, skillLevel);
+        }
+    }
+}
