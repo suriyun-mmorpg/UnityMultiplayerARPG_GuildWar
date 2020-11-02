e3d5d1fea48cdf53dfcde5954cbcf22738d65b60
Insthync <ittipon.bay@gmail.com>
2020-11-03 04:25:22 +0700

    Don't save character current position while in guild war map

--- Scripts/GuildWarMapInfo.cs
+++ Scripts/GuildWarMapInfo.cs
@@ -38,6 +38,7 @@ namespace MultiplayerARPG.MMO.GuildWar
         public string attackerWinMessage = "{0} can occupy the castle and win this round.";
 
         public override bool AutoRespawnWhenDead { get { return true; } }
+        public override bool SaveCurrentPosition { get { return false; } }
 
         public bool IsOn
         {
